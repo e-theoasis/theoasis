b1faf6f9723565e98c30cdccac63b9647023aebe
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-24 19:23:45 +0400

    Property Page update

--- client/src/pages/Property Page/PropertyPage.jsx
+++ client/src/pages/Property Page/PropertyPage.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './PropertyPage.css';
-import ContactForm from '../../components/ContactForm/ContactForm';
+import { properties } from '../../data/property';
 
 const PropertyPage =() => {
+
+    const property = properties[1]
+
     return (
         <div className="property-body">
+
+            {/* heading + slider */}
             <div className="landing-section">
+                {/* heading */}
+                <div className="property-name">
+                    <h1>{property.property_title}</h1>
+                </div>
             </div>
 
             <div className="intro-section">
@@ -21,12 +30,14 @@ const PropertyPage =() => {
             </div>
 
             <div className="amenities-section">
+
             </div>
 
             <div className="map-section">
             </div>
 
             <div className="community-section">
+
             </div>
         </div>
     );
