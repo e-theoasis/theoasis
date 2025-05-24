2799411139867e66b4304b672666b03cf6b7dc4f
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-24 19:23:23 +0400

    router linking

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -3,15 +3,21 @@ import Router from 'preact-router';
 import ContactForm from './components/ContactForm/ContactForm';
 import NavBar from './components/NavBar/NavBar';
 import HomePage from './pages/Homepage/HomePage';
+import PropertyPage from './pages/PropertyPage/PropertyPage'; // import this
 
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <NavBar />
+      
       <Router>
-        <PropertyPage path="/" />
+        <HomePage path="/" />
+        <PropertyPage path="/property/:slug" />
       </Router>
+
+      <ContactForm />
     </div>
   );
 }
--- client/src/pages/Homepage/HomePage.jsx
+++ client/src/pages/Homepage/HomePage.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import { Link } from 'preact-router'; //
+import { properties } from '../../data/properties';
 import './HomePage.css';
 import ContactForm from '../../components/ContactForm/ContactForm';
 
-const HomePage =() => {
-    return (
-        <div className="home-body">
-            <ContactForm />
-        </div>
-    );
+const HomePage = () => {
+  return (
+    <div className="home-body">
+      <div className="property-section">
+        {Object.entries(properties).map(([id, property]) => (
+          <Link href={`/property/${property.slug}`} key={id} className="property-link">
+            {property.property_title}
+          </Link>
+        ))}
+      </div>
+
+      <ContactForm />
+    </div>
+  );
 };
 
 export default HomePage;
