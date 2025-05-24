dd32d0968176c048189ddb8c9f733dc44ff4aa85
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-24 17:28:00 +0400

    Property 1 data

---
+++ client/src/data/property.js
@@ -0,0 +1,48 @@
+// PROPERTY ONE IMAGES
+import p1_landing1 from '../assets/property/one/landing-1.jpeg';
+import p1_landing2 from '../assets/property/one/landing-2.jpeg';
+import p1_landing3 from '../assets/property/one/landing-3.jpeg';
+import p1_landing4 from '../assets/property/one/landing-4.jpeg';
+import p1_logo from '../assets/property/one/logo.jpg';
+import p1_about1 from '../assets/property/one/about1.jpg';
+import p1_about2 from '../assets/property/one/about2.jpg';
+import p1_gallery1_1 from '../assets/property/one/gallery1-1.jpg';
+import p1_gallery1_2 from '../assets/property/one/gallery1-2.jpg';
+import p1_gallery1_3 from '../assets/property/one/gallery1-3.jpg';
+import p1_gallery1_4 from '../assets/property/one/gallery1-4.jpg';
+import p1_gallery1_5 from '../assets/property/one/gallery1-5.jpg';
+import p1_gallery1_6 from '../assets/property/one/gallery1-6.jpg';
+import p1_gallery2_1 from '../assets/property/one/gallery2-1.jpg';
+import p1_gallery2_2 from '../assets/property/one/gallery2-2.jpg';
+import p1_gallery2_3 from '../assets/property/one/gallery2-3.jpg';
+
+export const properties = {
+
+    // PROPERTY ONE DATA
+    1: {
+        slug: 'palace-villas',
+        property_title: 'Palace Villas – Ostra at The Oasis',
+        property_landing_images: [p1_landing1, p1_landing2, p1_landing3, p1_landing4], //slider
+        property_logline: 'Ultimate Exclusive Waterfront Escape',
+        property_intro: 'Welcome to Palace Villas – Ostra, an exclusive collection of waterfront homes within the Palace-branded community in The Oasis. Designed for those who seek a life shaped by luxury, calm, and connection to nature. Set along quiet shores and framed by pristine waters and verdant landscapes, each villa is a private retreat — where elegant architecture meets open, light-filled interiors.',
+        property_logo: p1_logo,
+        property_about1_title: 'Balance, Reimagined',
+        property_about1_text: 'Step into a sanctuary where wellness is a ritual and way of being. Wander through lush gardens, relax by crystalline waters, or find calm in moments of mindful pause — a space where balance isn’t found, but felt.',
+        property_about1_image: p1_about1,
+        property_about2_title: 'Playground for the Senses',
+        property_about2_text: 'From sunrise swims to starlit tennis, every space is an invitation to play, move, and revive. Whether you’re chasing adrenaline or calm, Palace Villas is where elegance meets effortless living.',
+        property_about2_image: p1_about2,
+        property_info_title: 'Your Exclusive Waterfront Sanctuary',
+        property_info_logline: 'Elegance in Every Wave',
+        property_info_text: 'A rare waterfront sanctuary where elegance flows as effortlessly as the tides. Palace Villas – Ostra reimagines luxury living with contemporary villas shaped by nature — framed by water, wrapped in greenery, and filled with light. A place where every day begins in beauty and ends in calm.',
+        property_info_stat1: '4 to 6-bedroom villas',
+        property_info_stat2: 'Starting Price 13.13 Mn AED',
+        property_info_stat3: '',
+        property_gallery1_title: 'Sculpted by Water, Shaped by Nature',
+        property_gallery1_images: [p1_gallery1_1, p1_gallery1_2, p1_gallery1_3, p1_gallery1_4, p1_gallery1_5, p1_gallery1_6], //slider
+        property_gallery2_title: 'Crafted for the Visionaries',
+        property_gallery2_images: [p1_gallery2_1, p1_gallery2_2, p1_gallery2_3], //slider
+        property_map: ''
+    }
+    
+}
