a372e1e8db9dae8dcab35ca10978e63dcbf08236
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-24 13:58:43 +0400

    Frontend update

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -19,7 +19,7 @@ const ContactForm = () => {
             message,
         };
 
-        const res = await fetch("https://the-oasis-backend-gwng.onrender.com/api/messages", {
+        const res = await fetch(`${import.meta.env.VITE_API_URL}/api/messages`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data),
