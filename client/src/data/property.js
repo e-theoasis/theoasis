196127ff1656340fcf856625210328202ef6cb59
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-24 19:23:32 +0400

    Property 2 data

--- client/src/data/property.js
+++ client/src/data/property.js
@@ -16,6 +16,25 @@ import p1_gallery2_1 from '../assets/property/one/gallery2-1.jpg';
 import p1_gallery2_2 from '../assets/property/one/gallery2-2.jpg';
 import p1_gallery2_3 from '../assets/property/one/gallery2-3.jpg';
 
+// PROPERTY TWO IMAGES
+import p2_landing1 from '../assets/property/two/landing-1.jpeg';
+import p2_landing2 from '../assets/property/two/landing-2.jpeg';
+import p2_landing3 from '../assets/property/two/landing-3.jpeg';
+import p2_landing4 from '../assets/property/two/landing-4.jpeg';
+import p2_landing5 from '../assets/property/two/landing-5.jpeg';
+import p2_logo from '../assets/property/two/logo.jpg';
+import p2_about1 from '../assets/property/two/about1.jpg';
+import p2_about2 from '../assets/property/two/about2.jpg';
+import p2_gallery1_1 from '../assets/property/two/gallery1-1.jpg';
+import p2_gallery1_2 from '../assets/property/two/gallery1-2.jpg';
+import p2_gallery1_3 from '../assets/property/two/gallery1-3.jpg';
+import p2_gallery1_4 from '../assets/property/two/gallery1-4.jpg';
+import p2_gallery1_5 from '../assets/property/two/gallery1-5.jpg';
+import p2_gallery2_1 from '../assets/property/two/gallery2-1.jpg';
+import p2_gallery2_2 from '../assets/property/two/gallery2-2.jpg';
+import p2_gallery2_3 from '../assets/property/two/gallery2-3.jpg';
+import p2_gallery2_4 from '../assets/property/two/gallery2-4.jpg';
+
 export const properties = {
 
     // PROPERTY ONE DATA
@@ -43,6 +62,32 @@ export const properties = {
         property_gallery2_title: 'Crafted for the Visionaries',
         property_gallery2_images: [p1_gallery2_1, p1_gallery2_2, p1_gallery2_3], //slider
         property_map: ''
+    },
+
+    // PROPERTY TWO DATA
+    2: {
+        slug: 'address-villas',
+        property_title: 'Address Villas – Tierra',
+        property_landing_images: [p2_landing1, p2_landing2, p2_landing3, p2_landing4, p2_landing5], //slider
+        property_logline: 'Luxe Living, Waterfront Retreat',
+        property_intro: 'Address Villas – Tierra is an exclusive collection of branded residences, where crystal-clear waters and vibrant greenery surround you, evoking a resort-style way of life. This blissful community offers sweeping waterfront views, light-filled spaces, and seamless access to nature. Embrace the beauty of waterside living, where each moment is designed for relaxation and rejuvenation.',
+        property_logo: p1_logo,
+        property_about1_title: 'Balance Begins here',
+        property_about1_text: 'Escape the ordinary and step into a world of wellness at Address Villas – Tierra. Enveloped by soothing waterscapes and blooming greenery, this is where your mind finds stillness, and your spirit recharges. Whether you seek to unwind through a relaxing walk, reflective meditation, or moments of quiet by the water, every day offers a new way to nurture your well-being.',
+        property_about1_image: p2_about1,
+        property_about2_title: 'Awaken Your Senses, Elevate Your Life',
+        property_about2_text: 'Relax by the pool, rejuvenate in the state-of-the-art gym, perfect your tennis game on world-class courts, and refresh your spirit with invigorating water experiences. Every corner is thoughtfully designed to energise your body, restore balance, and inspire a vibrant, holistic lifestyle.',
+        property_about2_image: p2_about2,
+        property_info_title: 'The Oasis of Tomorrow',
+        property_info_logline: 'Address Villas – Tierra',
+        property_info_text: 'Every curve, corner, and corridor is thoughtfully crafted to inspire. At Tierra, light, water, and landscape converge in perfect harmony, shaping a sensory retreat that calms the mind and restores the soul—a private world designed for pause, peace, and perfect stillness.',
+        property_info_stat1: '4 to 6-bedroom villas',
+        property_info_stat2: 'Swimmable Lagoon',
+        property_info_stat3: 'Starting Price 13.16 MN AED',
+        property_gallery1_title: 'Harmonizing Spaces, Water Inspired',
+        property_gallery1_images: [p2_gallery1_1, p2_gallery1_2, p2_gallery1_3, p2_gallery1_4, p2_gallery1_5], //slider
+        property_gallery2_title: 'Elegance in Every Wave, Calm in Every Detail',
+        property_gallery2_images: [p2_gallery2_1, p2_gallery2_2, p2_gallery2_3, p2_gallery2_4], //slider
+        property_map: ''
     }
-    
 }
