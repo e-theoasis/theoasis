09a80fa460e81f1d5c1acdc34d7357148b5fb4d8
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-25 21:47:33 +0400

    Contact Form update

--- client/src/components/ContactForm/ContactForm.jsx
+++ client/src/components/ContactForm/ContactForm.jsx
@@ -39,32 +39,21 @@ const ContactForm = () => {
     };
 
     return (
-        <div className="contact-form-body">
+        <div className="contact-form-body" id="contactform">
             <div className="contact-form-container">
                 <div className="contact-form-box">
                     <div className="left">
                     </div>
-
                     <div className="right">
-
                         <h2>Contact Us</h2>
-
                         <form onSubmit={handleSubmit}>
-
                             <input type="text" className="field" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required/>
-
                             <input type="email" className="field" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-
                             <input type="text" className="field" placeholder="Phone Number" value={phone} onChange={(e) => setPhone(e.target.value)} required pattern="[0-9]{10,11}" title="Please enter a valid phone number" />
-
                             <textarea className="field area" placeholder="Message" value={message} onChange={(e) => setMessage(e.target.value)} required></textarea>
-
                             <button className="btn" type="submit">Send</button>
-
                         </form>
-
                         {successMsg && <p className="success-msg"> {successMsg} </p>} 
-
                     </div>
                 </div>
             </div> 
