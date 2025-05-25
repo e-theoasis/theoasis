4215f0b7f4601652c27eec857387ea830c8d00c2
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-25 21:53:04 +0400

    FAQs data module created

---
+++ client/src/data/faqs.js
@@ -0,0 +1,26 @@
+export const FAQs = [
+  {
+    question: "WHAT ARE THE KEY HIGHLIGHTS OF THE OASIS BY EMAAR?", 
+    answer: "The key highlights include stunningly designed villas, gated community for tranquillity, prime location with seamless connectivity, vibrant community engagement, meticulously crafted water bodies including lagoons, and exceptional amenities."
+  },
+  {
+    question: "WHAT TYPES OF PROPERTIES ARE AVAILABLE AT THE OASIS?",
+    answer: "The Oasis is a unique residential development that offers spacious villas, townhouses, luxurious mansions and super-mansions with 4 & 5 bedrooms, ranging from 7,940 to 8,260 square feet, designed to redefine luxury living."
+  },
+  {
+    question: "WHAT AMENITIES ARE AVAILABLE FOR RESIDENTS AT THE OASIS?",
+    answer: "Spanning over 100 million square feet, The Oasis offers residents a vast and expansive living environment, with ample space for leisure and recreation. Enjoy beautifully landscaped parks within the community, providing residents with serene spaces to relax and unwind. Stay active with dedicated jogging tracks, perfect for morning runs or leisurely strolls amidst picturesque surroundings. Convenient access to local mosques ensures residents can fulfil their spiritual needs within the community. Access to pristine beaches allows residents to enjoy the sun, sand, and sea, enhancing the leisure opportunities within The Oasis. Access to world-class health facilities ensures residents have access to top-notch medical care within proximity."
+  },
+  {
+    question: "WHERE IS THE OASIS LOCATED?",
+    answer: "The Oasis is strategically located with easy access to key destinations such as Burj Khalifa, Downtown Dubai, Dubai International Airport, Sheikh Zayed Road, and The Dubai Mall."
+  },
+  {
+    question: "ARE THERE ANY SCHOOLS OR EDUCATIONAL INSTITUTIONS NEAR TO THE OASIS BY EMAAR?",
+    answer: "Yes, The Oasis is conveniently located near reputable schools and educational institutions, providing families with access to quality education options for their children."
+  },
+  {
+    question: "HOW IS THE CONNECTIVITY OF THE OASIS TO MAJOR TRANSPORTATION ROUTES?",
+    answer: "The Oasis enjoys superb road connectivity, with proximity to major transportation routes such as Sheikh Zayed bin Hamdan Road (D54) and the future extension of Al Khail Road (E44)."
+  }
+]
