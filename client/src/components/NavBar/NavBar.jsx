9daa0ccca965593904434f95a24a5cbf59c15529
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-25 21:51:47 +0400

    NavBar component

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -4,6 +4,18 @@ import './NavBar.css';
 const NavBar =() => {
     return (
         <div className="navbar-body">
+            <header>
+                <h1 id="logo"><a href="#home-body">EMAAR</a></h1>
+                <nav>
+                    <ul id="navbar">
+                        <li><a href="#intro">ABOUT US</a></li>
+                        <li><a href="#properties">PROPERTIES</a></li>
+                        <li><a href="#communities">COMMUNITIES</a></li>
+                        <li><a href="#location">LOCATION</a></li>
+                    </ul>
+                </nav>
+                <button><a href="#contactform">GET IN TOUCH</a></button>
+            </header>
 
         </div>
     );
