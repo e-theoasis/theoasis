0cf97e2a838c8e738b441607e71de38a9b37021c
Syeda Hafsa <e.theoasis.properties@gmail.com>
2025-05-26 11:06:06 +0400

    Update app.jsx
    Routing

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,23 +1,26 @@
 import { useState } from 'preact/hooks';
-import Router from 'preact-router';
-import ContactForm from './components/ContactForm/ContactForm';
-import NavBar from './components/NavBar/NavBar';
+import { Router } from 'preact-router';
 import HomePage from './pages/Homepage/HomePage';
-import PropertyPage from './pages/PropertyPage/PropertyPage'; // import this
+import PropertyPage from './pages/PropertyPage/PropertyPage';
+import NavBar from './components/NavBar/NavBar'; 
+import ContactForm from './components/ContactForm/ContactForm';
 import Footer from './components/Footer/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 export function App() {
   const [count, setCount] = useState(0);
+  const [currentPath, setCurrentPath] = useState('/');
 
   return (
-    <div className="App" id='complete'> 
-        < NavBar />  
-        <Router>
+    <div className="App" id='complete'>  
+        < NavBar />
+        <Router onChange={({url}) => setCurrentPath(url)}>
           <HomePage path="/" />
           <PropertyPage path="/property/:slug" />
         </Router>
         <ContactForm />
         <Footer />
+        <ScrollToTop path={currentPath}/>
     </div>
   );
 }
